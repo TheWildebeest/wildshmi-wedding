6e07eb4baa3b54274c7f2d98e584e01280c6e88e
David M Wildman <david.m.wildman.dev@gmail.com>
2023-03-19 17:34:05 +0000

    add Heading component

---
+++ src/DocumentHead.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export const DocumentHead = ({children}: { children?: ReactNode }) => {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="David and Lakshmi's wedding website" />
+      <link rel="icon" href="/favicon.ico" />
+      {children}
+    </>
+  )
+}
---
+++ src/shared/components/HeadingOne.tsx
@@ -0,0 +1,27 @@
+import localFont from "@next/font/local";
+import { FC, PropsWithChildren } from "react";
+
+const annissaScript = localFont({ src: "../../../public/assets/fonts/annisa.otf"});
+
+export const HeadingOne = (props: PropsWithChildren) => {
+  const classNames = [
+    annissaScript.className,
+    'text-center',
+    'text-4xl',
+    'sm:text-5xl',
+    'md:text-6xl',
+    'lg:text-7xl',
+    'xl:text-8xl',
+    'w-100',
+    'font-medium',
+    'py-8',
+    'my-8',
+  ].join(' ');
+
+  const { children } = props;
+  return (
+    <div className={classNames}>
+      <h1 className='w-4/5 lg:w-3/5 mx-auto'>{ children }</h1>
+    </div>
+  )
+}
