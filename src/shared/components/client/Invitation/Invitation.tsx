3b77bb324bef9813393e54118ddd914eee8ed439
David Wildman <david.m.wildman@gmail.com>
2023-05-01 23:14:07 +0100

    fix invitation page errors

--- src/shared/components/client/Invitation/Invitation.tsx
+++ src/shared/components/client/Invitation/Invitation.tsx
@@ -1,23 +1,31 @@
-"use client"
-import { Data } from '@/pages/api/invitation';
-import { Text } from '@/shared/components/Text'
-import { EventNames } from '@/shared/models';
-import { InvitationContext } from '@/shared/providers/InvitationContext';
-import { use, useContext } from 'react';
-import fonts from '@/shared/fonts';
+"use client";
+import { Text } from "@/shared/components/Text";
+import { EventNames } from "@/shared/models";
+import { InvitationContext } from "@/shared/providers/InvitationContext";
+import { useContext } from "react";
+import fonts from "@/shared/fonts";
 
 export const Invitation = () => {
   const { arima } = fonts;
-  const eventName = EventNames[(useContext(InvitationContext))]
+  const eventName = EventNames[useContext(InvitationContext)];
   // const res: Response = use(fetch("http://localhost:3000/api/invitation"));
   // const data: Data = use(res.json());
   // console.log('Fetched data: ', data.url)
   return (
-    <div className={'text-center ' + arima}>
-      <Text tagName='div' size='M'>You are warmly invited to join</Text>
-      <Text tagName='div' size='XXL'>David <strong style={{ color: 'var(--wildshmi-primary)'}}>&</strong> Lakshmi</Text>
-      <Text tagName='div' size='M'>at our {eventName} on</Text>
-      <Text tagName='div' size='XL'>26 August 2023</Text>
+    <div className={"text-center " + arima}>
+      <Text tagName="div" size="M">
+        You are warmly invited to join
+      </Text>
+      <Text tagName="div" size="XXXL">
+        David <strong style={{ color: "var(--wildshmi-primary)" }}>&</strong>{" "}
+        Lakshmi
+      </Text>
+      <Text tagName="div" size="M">
+        at their {eventName} on
+      </Text>
+      <Text tagName="div" size="XXL">
+        26 August 2023
+      </Text>
     </div>
-  )
-}
+  );
+};
