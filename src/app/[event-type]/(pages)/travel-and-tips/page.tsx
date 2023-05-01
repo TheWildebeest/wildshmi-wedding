2d31723863e6a63b52661cfc75f1d8f7ea31a93f
David Wildman <david.m.wildman@gmail.com>
2023-05-01 23:16:53 +0100

    start travel and tips page

--- src/app/[event-type]/(pages)/travel-and-tips/page.tsx
+++ src/app/[event-type]/(pages)/travel-and-tips/page.tsx
@@ -1,5 +1,274 @@
-import React, { FC } from "react"
+import { AccessibleImage } from "@/shared/components/AccessibleImage";
+import { Paragraph } from "@/shared/components/Paragraph";
+import { Text } from "@/shared/components/Text";
+import fonts from "@/shared/fonts";
+import React, { FC } from "react";
 
-const TravelAndTipsPage: FC = () => <>{}</>
+const TravelAndTipsPage: FC = () => {
+  const { merienda } = fonts;
 
+  return (
+    <div className="max-w-[50em] mx-auto">
+      <Paragraph>
+        As you prepare for your trip to the bustling city of London for our
+        upcoming wedding, we&lsquo;ve put together some travel tips and
+        recommendations to help you make the most of your time here.
+      </Paragraph>
+      <Text tagName="h2" size="L">
+        🛬 International arrivals
+      </Text>
+      <Paragraph>
+        Most international flights terminate at Gatwick or Heathrow, which are
+        the most convenient airports to fly in to.
+      </Paragraph>
+      <Paragraph>
+        Both airports have an express train service connecting to central
+        London, for which you have to buy a ticket in advance, in addition to
+        regular train services.
+      </Paragraph>
+      <Text tagName="h3" size="M">
+        Gatwick
+      </Text>
+      <Paragraph></Paragraph>
+      <Paragraph>
+        You can also just take a regular train service, and use your contactless
+        card to tap in at the station. Look for the Elizabeth Line at Heathrow,
+        or at Gatwick follow the signs for Trains and the South Terminal.
+      </Paragraph>
+      <Text tagName="h3" size="M">
+        Heathrow
+      </Text>
+      <Paragraph>
+        You ca Stansted and Luton are two other airports which also serve
+        London-bound travellers. From there, you can take a taxi or public
+        transportation such as the Tube or a train to your hotel or
+        accommodation. Contactless card payment is accepted at the fairgates and
+        in taxis.
+      </Paragraph>
+      <Paragraph>
+        Parents will frequently bestow daughters with this providential namesake
+        in the hope of manifesting a destiny that sees them grow up to become
+        rich and successful.
+      </Paragraph>
+      <Paragraph>
+        Imagine, growing up seeing your name associated primarily with a literal
+        goddess who has gold coins pouring out of her hands. You&lsquo;d be
+        forgiven for occasionally wondering whether this might be a hard act to
+        follow.
+      </Paragraph>
+      <Paragraph>
+        Not so for <i>this</i> Lakshmi, who has made it her life&lsquo;s goal to
+        place the mother of all ticks in the archetypes box by pursuing a career
+        in investment banking. At the time of writing, she&lsquo;s a VP in
+        Electronic Trading at Bank of America. It sounds impressive, though
+        no-one actually knows what it means.
+      </Paragraph>
+      <Paragraph>
+        Born in Thiruvananthapuram, capital of the state of Kerala in India,
+        Lakshmi is a true citizen of the world. She spent her childhood between
+        India, South Africa, and Oman, and left to study in Singapore, where she
+        then worked for a number of years. In 2017 she moved to Hong Kong for a
+        couple of years, before relocating to London.
+      </Paragraph>
+      <Paragraph>
+        Lakshmi had been something of a rebel during her school days. When the
+        time came to apply to universities, a family friend suggested she apply
+        to a relatively mediocre university (NTU) in Singapore. Instead, she
+        ended up accidentally applying to one of the top ten Engineering courses
+        in the world, and winning a full scholarship that propelled her to
+        success. As an Alumnus of the National University of Singapore, Lakshmi
+        lays claim to a status whose pedagogical pedigree is as impressive as
+        its acronym is unfortunate.
+      </Paragraph>
+      <Paragraph>
+        What does a bona-fide goddess of wealth look for in a man, than?
+        Fortunately for David, she cares more about personality than paycheck,
+        and even then, the personality requirements aren&lsquo;t particularly
+        stringent. From as early as she can remember, Lakshmi has always known
+        what see wanted in a man. At school when her friends would ask her what
+        type of guy she wanted to marry, her answer was always the same:
+        &ldquo;A funny guy&rdquo;.
+      </Paragraph>
+      <Text tagName="h3" size="M">
+        David&lsquo;s origin story
+      </Text>
+      <Paragraph>
+        Though not a golden goddess, David&lsquo;s name also comes with a hefty
+        pedigree — that of David, King of Israel, from the Bible. The story goes
+        that he rose from humble origins as a shepherd boy and there&lsquo;s a
+        famous story about him using his shepherd&lsquo;s slingshot to kill the
+        phillistine giant Goliath.
+      </Paragraph>
+      <Paragraph>
+        Perhaps the main area where the regal and non-regal Davids differ is in
+        the caliber of giants they heave defeated. David reluctantly concedes
+        the point that demolishing a footlong Subway sandwich really isn&lsquo;t
+        as impressive as toppling Goliath.
+      </Paragraph>
+      <Paragraph>
+        Much like his namesake, though, David loves playing stringed
+        instruments, the guitar in particular. He&lsquo;s learned to play one or
+        two famous guitar solos quite well, and he is quite pretty chuffed with
+        this achievement.
+      </Paragraph>
+      <Paragraph>
+        David was born in Liverpool and grew up on the Isle of Man. After
+        visiting China at the age of 16, he opted to study Chinese at the
+        University of Cambridge. Cambridge was the sixth best university in the
+        world during David&lsquo;s time there. Thankfully, nearly a decade on,
+        Cambridge has recovered from the impact of David&lsquo;s tenure, and it
+        now comes in at third place.
+      </Paragraph>
+      <Paragraph>
+        Starting his career in media sales, David spent three years selling
+        digital advertising to clients in Jersey, Guernsey, London, and even the
+        Isle of Man; eventually branching out further afield and visiting
+        Moscow, Kuala Lumpur, Beijing, and Shanghai.
+      </Paragraph>
+      <Paragraph>
+        It was as stressful as it sounds, and David is relieved to be on the
+        other side of a career change which he started in early 2019. Starting
+        from scratch with a course in software development that he completed
+        during the first lockdown, David is now gainfully employed in his second
+        Software Developer role. He loves JavaScript almost as much as he once
+        loved Mandarin Chinese, but far less than he loves his golden goddess
+        Lakshmi.
+      </Paragraph>
+      <Text tagName="h2" size="M">
+        How they met
+      </Text>
+      <Paragraph className={merienda}>
+        &ldquo;We met the old fashioned way — on Tinder!&rdquo;
+      </Paragraph>
+      <Paragraph>
+        Lakshmi and David have been together (pretty much literally) from when
+        they first met in the Three Tuns pub in Aldgate East in London, on
+        November 3rd 2020.
+      </Paragraph>
+      <Paragraph>
+        With nowhere to sit inside, they took their pints outside to the beer
+        garden, with nothing but the warmth of each others&lsquo; company and an
+        array of carefully positioned halogen lamps to ward off the chill of the
+        November air.
+      </Paragraph>
+
+      <Paragraph>
+        They took an instant liking to one another. Lakshmi was impressed by
+        David&lsquo;s biceps, and David was bowled over by Lakshmi&lsquo;s
+        generous compliments.
+      </Paragraph>
+      <Paragraph>
+        Lakshmi approached David with a proposition that he simply
+        couldn&lsquo;t refuse. She told him she had an Xbox One and a new game
+        had just come out that was co-operative multiplayer.
+      </Paragraph>
+      <Paragraph>
+        &ldquo;Beer and video games !? I&lsquo;d better hold on to this
+        one!&rdquo;, David thought to himself.
+      </Paragraph>
+      <Paragraph>
+        Over the next two months, David and Lakshmi got to know each other as
+        Leo and Vincent, the playable protagonist characters in A Way Out. As
+        Leo and Vincent, they met each other for the first time again, escaped
+        from prison together, evaded police, and tracked down a crime boss, all
+        while navigating the twists and turns of the characters&lsquo;
+        relationship.
+      </Paragraph>
+      <Paragraph>
+        The following two years, meanwhile, have seen David and Lakshmi get to
+        know each other in real life. This has thankfully featured far less
+        violence and fewer encounters with police (apart from that one time).
+        Nevertheless, the journey has been equally if not more exhilarating, as
+        they stepped outside their comfort zones, daring to trust and support
+        one another enough to be able to start planning a future together.
+      </Paragraph>
+      <Paragraph>
+        In many ways, it has been all-or-nothing since the start. The day after
+        their first date, a national lockdown came into effect. This was the
+        second such lockdown of the Covid-19 pandemic, and though we all knew
+        the drill by this point, Lakshmi and David were faced with forming a
+        &ldquo;support bubble&rdquo; in order to be able to continue seeing each
+        other, or waiting until after lockdown. Who would have known that the
+        humble bubble which once supported Lakshmi and David&lsquo;s nascent
+        relationship was to be a progenitor of nuptials?
+      </Paragraph>
+      <Paragraph>
+        Because of the lockdown, their first months were spent in close quarters
+        at each others&lsquo; flats, playing games, cooking exotic meals
+        together, watching TV shows. It was only later as restrictions eased
+        that they began to venture out to explore the streets of East London,
+        visiting the docks to feed the ducks, and taking evening strolls by the
+        river and across Tower Bridge.
+      </Paragraph>
+      <Paragraph>
+        In the summer of 2022 they had the opportunity to meet each
+        others&lsquo; parents, with Haridas and Latha flying over from
+        Trivandrum and Chris and Martine from the Isle of Man, to spend some
+        time testing the waters as in-laws. They knew it would work out when
+        David cooked dinner for the parents one evening and brought together
+        both mothers&lsquo; signature recipes in one meal — Martine&lsquo;s
+        lentil pasta sauce taking on a new role as daal, alongside Latha&lsquo;s
+        lamb biryani.
+      </Paragraph>
+
+      <Text tagName="h2" size="M">
+        Getting engaged
+      </Text>
+      <Paragraph>
+        The decision to get married happened organically and gradually as
+        Lakshmi and David got to know each other, but as 2022 drew to a close
+        they started thinking of making it a reality.
+      </Paragraph>
+      <Paragraph>
+        2023 kicked off with choosing the date, and as February edged ever
+        closer, David started planning a proper proposal to surprise Lakshmi
+        with a ring on Valentine&lsquo;s day.
+      </Paragraph>
+      <Paragraph>
+        Secretly booking the day off work for both of them, David arrange a day
+        full of romantic activities — breakfast at sunrise in a fortieth-floor
+        restaraunt, a massage workshop with roses, prosecco, and chocolates, a
+        game of chess which <del>he let Lakshmi win</del>{" "}
+        <ins
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Lakshmi won fair and square
+        </ins>
+        , and an afternoon tea with a special diamond-based dessert!
+      </Paragraph>
+      <Paragraph>
+        It was the perfect day and they celebrated afterwards with another
+        surprise — a trip back to where it all started at The Three Tuns, where
+        Lakshmi&lsquo;s friends and colleagues were waiting with prosecco on
+        standby to surprise her.
+      </Paragraph>
+      <Paragraph>
+        Going straight from one whirlwind experience to another, David and
+        Lakshmi then jetted off to India in March 2023 along with David&lsquo;s
+        parents, sister, and two brothers, for an &ldquo;official&rdquo;
+        engagement ceremony with Lakshmi&lsquo;s extended family. This
+        conveniently doubled up as a massive birthday bash for Lakshmi&lsquo;s
+        mother Latha.
+      </Paragraph>
+      <Paragraph>
+        You can check out the photos from the proposal and the engagement party
+        on the photos page.
+      </Paragraph>
+      <Paragraph>
+        Then, make sure you fill in the RSVP form so you can join Lakshmi and
+        David in creating more happy memories (and taking more awesome photos)
+        with them at the wedding on 26 August.
+      </Paragraph>
+
+      <Paragraph>
+        <span className={merienda}>
+          &ldquo;We can&lsquo;t wait to see you there!&rdquo;
+        </span>{" "}
+        — David and Lakshmi
+      </Paragraph>
+    </div>
+  );
+};
 export default TravelAndTipsPage;
