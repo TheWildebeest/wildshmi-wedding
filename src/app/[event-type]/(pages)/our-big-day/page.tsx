c0c466b94c24f9fac2f989c387556380afccbb99
David Wildman <david.m.wildman@gmail.com>
2023-05-03 00:20:29 +0100

    start timeline

--- src/app/[event-type]/(pages)/our-big-day/page.tsx
+++ src/app/[event-type]/(pages)/our-big-day/page.tsx
@@ -1,5 +1,16 @@
+import { Paragraph } from "@/shared/components/Paragraph";
+import { Timeline } from "@/shared/components/timeline";
 import React, { FC } from "react";
 
-const OurBigDayPage: FC = () => <>{}</>;
+const OurBigDayPage: FC = () => {
+  return (
+    <div className="max-w-[50em] mx-auto">
+      <Paragraph>
+        Here&lsquo;s a timeline so you know what to expect on the day!
+      </Paragraph>
+      <Timeline></Timeline>
+    </div>
+  );
+};
 
 export default OurBigDayPage;
