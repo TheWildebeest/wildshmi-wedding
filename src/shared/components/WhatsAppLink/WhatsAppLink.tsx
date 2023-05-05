45c229f039d8b9183c308d1b2ed21e47fc52fdf1
David Wildman <david.m.wildman@gmail.com>
2023-05-06 00:54:58 +0100

    create WhatsAppLink component

---
+++ src/shared/components/WhatsAppLink/WhatsAppLink.tsx
@@ -0,0 +1,33 @@
+import { Text } from "../Text";
+import whatsappicon from "public/assets/images/icons/whatsapp.svg";
+import { Icon } from "../Icon";
+
+export interface WhatsAppLinkProps {
+  name: string;
+  phoneNumber: string;
+}
+
+export const WhatsAppLink = ({ phoneNumber, name }: WhatsAppLinkProps) => {
+  const formattedPhoneNumber = phoneNumber
+    .split("")
+    .join("")
+    .replace(/\D/g, "");
+  const messageMeText = `Message ${name} on WhatsApp`;
+  const uriEncodedText = encodeURIComponent(
+    `Hello, it's ${name} here! Feel free to get in touch about the wedding.`
+  );
+  const href = `https://wa.me/${formattedPhoneNumber}?text=${uriEncodedText}`;
+  return (
+    <Text tagName="span" size="S">
+      WhatsApp:
+      <a
+        style={{ color: "black", textDecoration: "none" }}
+        aria-label={messageMeText}
+        href={href}
+      >
+        <Icon src={whatsappicon} altText={messageMeText}></Icon>
+        <strong>{phoneNumber}</strong>
+      </a>
+    </Text>
+  );
+};
