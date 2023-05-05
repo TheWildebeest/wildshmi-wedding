af38a3ea242d32a6d37fa5a21fc3ed2d165c7324
David Wildman <david.m.wildman@gmail.com>
2023-05-06 00:54:23 +0100

    create icon component

---
+++ src/shared/components/Icon/Icon.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from "next/image";
+
+export interface IconProps {
+  src: StaticImageData;
+  altText: string;
+}
+
+const Icon = ({ src, altText }: IconProps) => {
+  return (
+    <Image
+      style={{
+        display: "inline-block",
+        width: "1em",
+        margin: "0 0.4em 0em 0.6em",
+        verticalAlign: "middle",
+      }}
+      src={src}
+      alt={altText}
+    />
+  );
+};
+export { Icon };
