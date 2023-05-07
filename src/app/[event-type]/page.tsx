aee8546b101da57c44a84c030b742a35d33413a4
David Wildman <david.m.wildman@gmail.com>
2023-05-08 00:18:51 +0100

    rsvp button on homepage

--- src/app/[event-type]/page.tsx
+++ src/app/[event-type]/page.tsx
@@ -4,58 +4,19 @@ import { Text } from "@/shared/components/Text";
 import { FC } from "react";
 import Image from "next/image";
 import floralarrangement from "../../../public/assets/images/floral-arrangement.png";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import fonts from "@/shared/fonts";
+import { RsvpButton } from "@/shared/components/RSVPButton";
 
 const WeddingPage: FC = () => {
-  const basePath = usePathname();
-  const { arima } = fonts;
   return (
     <>
       <Invitation></Invitation>
-      <div className="text-center w-100" style={{ marginTop: "1rem" }}>
-        <Text tagName="div" size="XL">
-          <div>
-            <div
-              style={{
-                display: "flex",
-                placeItems: "center",
-                height: "2em",
-                top: "30%",
-                left: "0",
-                right: "0",
-                bottom: "0",
-                color: "white",
-              }}
-            >
-              <Link
-                style={{
-                  padding: "2% 0",
-                  margin: "0 auto",
-                  width: "30%",
-                  height: "max-content",
-                  background: "var(--wildshmi-secondary)",
-                  boxShadow: "var(--wildshmi-grey) 0 10px 20px -10px",
+      <RsvpButton></RsvpButton>
 
-                  borderRadius: "0.5em",
-                  color: "white",
-                  textDecoration: "unset",
-                }}
-                href={{ pathname: basePath + "/rsvp" }}
-              >
-                <span className={`${arima} font-semibold`}>RSVP</span>
-              </Link>
-            </div>
-          </div>
-        </Text>
-      </div>
       <div className="text-center w-100" style={{ marginTop: "1rem" }}>
         <Text tagName="div" size="XL">
           <div
             style={{
               margin: "0 auto",
-              // objectPosition: 'center center',
               objectFit: "cover",
               width: "10em",
               position: "relative",
@@ -65,11 +26,8 @@ const WeddingPage: FC = () => {
               src={floralarrangement}
               alt="Picture of a rose"
               style={{
-                // margin: '0 auto',
-                // objectPosition: 'center center',
                 objectFit: "cover",
                 width: "10em",
-                // filter: "drop-shadow(0px 0px 3px grey)",
               }}
             />
           </div>
