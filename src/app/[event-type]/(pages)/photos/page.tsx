100b16a70a2bd03cb83b7d4f4eb6afe366f6a093
David Wildman <david.m.wildman@gmail.com>
2023-05-08 20:05:56 +0100

    photos page

--- src/app/[event-type]/(pages)/photos/page.tsx
+++ src/app/[event-type]/(pages)/photos/page.tsx
@@ -1,5 +1,28 @@
+import { Text } from "@/shared/components/Text";
+import Link from "next/link";
 import React, { FC } from "react";
 
-const TravelAndTipsPage: FC = () => <>{}</>;
+const PhotosPage: FC = () => (
+  <>
+    <ul>
+      <li style={{ marginBottom: "1em" }}>
+        <Text size="M">
+          <Link href="https://www.facebook.com/LHaridas/posts/pfbid0jyDKT6DMVHoUZspEBm5rX55Lm2ugMBKiuH9V258gvtu4ukyR1UWEGRbDrJgK4oGxl">
+            Proposal and engagement party
+          </Link>{" "}
+          — London, 14.04.2023
+        </Text>
+      </li>
+      <li style={{ marginBottom: "1em" }}>
+        <Text size="M">
+          <Link href="https://www.facebook.com/LHaridas/posts/pfbid0ZbnxLztUxHtQwr1Z7J9FDDEWjXjVAtZCHUmzgLYh1nsvSweeSeUdzwwSqYQJvhBnl">
+            India engagement ceremony
+          </Link>{" "}
+          — Thiruvananthapuram, 11.03.2023
+        </Text>
+      </li>
+    </ul>
+  </>
+);
 
-export default TravelAndTipsPage;
+export default PhotosPage;
